381993a91d9268f9e27763886d8bef504f2eac29
oguzhanerdem96 <oguzhan_erdem96@hotmail.com>
2022-03-24 01:44:07 +0300

    git test

---
+++ autoLayoutCodeTestProject/MainVC.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  autoLayoutCodeTestProject
+//
+//  Created by Oğuzhan Erdem on 23.03.2022.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+let imageView = UIImageView(image: #imageLiteral(resourceName: "googleImage"))
+
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+        
+        
+    }
+
+
+}
+
